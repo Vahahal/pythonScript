c763b1a2c3dd71afbdb590826937fc53bb726131
Valentin Sabin <valentin_sabin@hotmail.fr>
2017-06-02 19:36:56 +0200

    Create vigenere.py

---
+++ vigenere.py
@@ -0,0 +1,128 @@
+import maya.cmds as mc
+
+
+def encode_vigenere(clear_txt, key):
+    txt = clear_txt.replace(" ", "")
+    #txt = clear_txt.remove(" ","'", ",", "?", ";", ".", ":", "(", ")", "/", "-", "+", "*", "1","2","3","4","5","6","7","8","9","0")
+    txt = txt.upper()
+    key = key.upper()
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    vigenere =[('B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A'),
+               ('C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B'),
+               ('D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C'),
+               ('E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D'),
+               ('F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E'),
+               ('G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F'),
+               ('H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G'),
+               ('I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'),
+               ('J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'),
+               ('K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'),
+               ('L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'),
+               ('M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'),
+               ('N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'),
+               ('O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N'),
+               ('P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'),
+               ('Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'),
+               ('R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q'),
+               ('S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R'),
+               ('T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'),
+               ('U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'),
+               ('V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'),
+               ('W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'),
+               ('X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'),
+               ('Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X'),
+               ('Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y'),
+               ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')]
+
+    crypted_txt = []
+    count = 0
+    for i in txt:
+        for j in vigenere:
+            if j[0] == key[count]:
+                crypted_txt.append(alphabet[j.index(i)])
+
+        count += 1
+        if count == len(key):
+            count = 0
+
+    crypted_txt = ''.join(crypted_txt)
+    return crypted_txt
+
+
+
+def uncode_vigenere(crypted_txt, key):
+    crypted_txt = crypted_txt.upper()
+    key = key.upper()
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z']
+    vigenere = []
+    ligne = []
+    for y in range(26):
+        for x in range(26):
+            ligne.append(chr(65 + (x + y) % 26))
+        vigenere.append(ligne)
+        ligne = []
+
+    uncrypted_txt = []
+    count = 0
+    for i in crypted_txt:
+        crypted_lettre = alphabet.index(i)
+
+        for j in vigenere:
+            if j[0] == key[count]:
+                uncrypted_txt.append(vigenere[vigenere.index(j)][crypted_lettre])
+
+        count += 1
+        if count == len(key):
+            count = 0
+
+    uncrypted_txt = ''.join(uncrypted_txt)
+    uncrypted_txt = uncrypted_txt.lower()
+    return uncrypted_txt
+
+
+def callBack_encode(w):
+    a = mc.textField ('clear_text', query = True, text = True)
+    b = mc.textField ('crypting_key', query = True, text= True)
+    result = encode_vigenere(a,b)
+    mc.textField('result_crypted', edit= True, text = result)
+
+
+def callBack_uncode(z):
+    x = mc.textField ('crypted_text', query = True, text = True)
+    y = mc.textField ('decrypting_key', query = True, text= True)
+    result = uncode_vigenere(x,y)
+    mc.textField('result_decrypted', edit= True, text = result)
+
+
+
+
+
+def show_facial_UI():
+    sWindow = 'facial_2D'
+    if mc.window(sWindow, q=True, ex=True):
+        mc.deleteUI(sWindow)
+
+    mc.window(sWindow, t='Vigenere crypting')
+    mc.columnLayout(columnAttach=('both', 5), rowSpacing=10, columnWidth=300)
+    mc.separator(h=25)
+    mc.text(label='Encrypt')
+    mc.text(label='Enter your clear text')
+    mc.textField('clear_text', width = 100)
+    mc.text(label='Enter the key')
+    mc.textField('crypting_key')
+    mc.button(w=100, label='Encrypt', command= callBack_encode)
+    mc.textField('result_crypted', editable = False)
+    mc.separator(h=10, style='in')
+    mc.text(label='Uncrypt')
+    mc.text(label='Enter your crypted text')
+    mc.textField('crypted_text')
+    mc.text(label='Enter the key')
+    mc.textField('decrypting_key')
+    mc.button(w=100, label='Encrypt', command= callBack_uncode)
+    mc.textField('result_decrypted', editable = False)
+
+    mc.showWindow(sWindow)
+
+
+show_facial_UI()
